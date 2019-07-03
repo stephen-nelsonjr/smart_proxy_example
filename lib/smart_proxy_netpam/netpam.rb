1fa8774639df753f2e48c5647b5f7cc52adf3be3
stephen-nelsonjr <47092375+stephen-nelsonjr@users.noreply.github.com>
2019-07-03 08:54:33 -0400

    Update netpam.rb

--- lib/smart_proxy_netpam/netpam.rb
+++ lib/smart_proxy_netpam/netpam.rb
@@ -2,11 +2,11 @@ module Proxy::Example
   class NotFound < RuntimeError; end
 
   class Plugin < ::Proxy::Plugin
-    plugin 'netpam', Proxy::Netpam::VERSION
+    plugin 'netbox', Proxy::Netbox::VERSION
 
     default_settings :hello_greeting => 'O hai!'
 
-    http_rackup_path File.expand_path('netpam_http_config.ru', File.expand_path('../', __FILE__))
-    https_rackup_path File.expand_path('netpam_http_config.ru', File.expand_path('../', __FILE__))
+    http_rackup_path File.expand_path('netbox_http_config.ru', File.expand_path('../', __FILE__))
+    https_rackup_path File.expand_path('netbox_http_config.ru', File.expand_path('../', __FILE__))
   end
 end
