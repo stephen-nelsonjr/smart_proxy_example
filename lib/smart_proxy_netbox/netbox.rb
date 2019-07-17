8778b087eb2d33d8c5f4c399fecdb9643f38fdf4
stephen-nelsonjr <47092375+stephen-nelsonjr@users.noreply.github.com>
2019-07-17 09:45:49 -0400

    Update netbox.rb

--- lib/smart_proxy_netbox/netbox.rb
+++ lib/smart_proxy_netbox/netbox.rb
@@ -4,7 +4,7 @@ module Proxy::Example
   class Plugin < ::Proxy::Plugin
     plugin 'netbox', Proxy::Netbox::VERSION
 
-    default_settings :hello_greeting => 'O hai!'
+#     default_settings :hello_greeting => 'O hai!'
 
     http_rackup_path File.expand_path('netbox_http_config.ru', File.expand_path('../', __FILE__))
     https_rackup_path File.expand_path('netbox_http_config.ru', File.expand_path('../', __FILE__))
