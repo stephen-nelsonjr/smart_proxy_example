b4b9483f495f95e9814b4c3dbd8b8fb777d1f148
stephen-nelsonjr <47092375+stephen-nelsonjr@users.noreply.github.com>
2019-07-18 08:28:05 -0400

    Update netbox_client.rb

--- lib/smart_proxy_netbox/netbox_client.rb
+++ lib/smart_proxy_netbox/netbox_client.rb
@@ -2,14 +2,14 @@ require 'yaml'
 require 'logger'
 require 'json' 
 require 'net/http'
-require 'smart_proxy_ipam/ipam'
-require 'smart_proxy_ipam/ipam_main'
+require 'smart_proxy_netbox/netbox'
+require 'smart_proxy_netbox/netbox_main'
 
 module Proxy::Netbox
   class NetboxClient
     def initialize 
-      conf = Proxy::Ipam.get_config[:phpipam]
-      @phpipam_config = {
+      conf = Proxy::Ipam.get_config[:netbox]
+      @netbox_config = {
         :url => conf[:url], 
         :user => conf[:user], 
         :password => conf[:password]
