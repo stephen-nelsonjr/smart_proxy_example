faedd669dbde9015f4fc4e11e06c3a2a5ca385cc
stephen-nelsonjr <47092375+stephen-nelsonjr@users.noreply.github.com>
2019-07-29 13:47:20 -0400

    Update netbox_client.rb

--- lib/smart_proxy_netbox/netbox_client.rb
+++ lib/smart_proxy_netbox/netbox_client.rb
@@ -25,7 +25,7 @@ module Proxy::Netbox
     end
     
     def add_prefix(prefix, description)
-      # create new prefix
+      # Create new Prefix
       new_prefix = NetboxClientRuby::IPAM::Prefix.new
       new_prefix.prefix = "123.1.0.0/24" #String
       new_prefix.description = description #String
@@ -36,6 +36,7 @@ module Proxy::Netbox
     end
     
     def add_ip(address, description)
+      # Create new IP Address
       new_ip = NetboxClientRuby::IPAM::IpAddress.new
       new_ip.address = address #"192.1.0.2/16" #String
       new_ip.status = "Active" 
@@ -44,6 +45,7 @@ module Proxy::Netbox
     end
     
     def add_vlan_role(name, slug)
+      # Create new VLAN Role
       new_role = NetboxClientRuby::IPAM::Role.new
       new_role.name = name #String
       new_role.slug = slug #String 
@@ -51,6 +53,7 @@ module Proxy::Netbox
     end
     
     def add_aggregate(prefix, rir)
+      # Create new Agggregrate
       new_agg = NetboxClientRuby::IPAM::Aggregrate.new
       new_agg.prefix = prefix #String
       new_agg.rir = rir #integer
@@ -58,6 +61,7 @@ module Proxy::Netbox
     end
     
     def add_rir(name, slug)
+      # Create new RIR
       new_rir = NetboxClientRuby::IPAM::Rir.new
       new_rir.name = name #String
       new_rir.slug = slug #String
@@ -65,12 +69,14 @@ module Proxy::Netbox
     end
     
     def add_vrf(name)
+      # Create new VRF
       new_vrf = NetboxClientRuby::IPAM::Vrf.new
       new_vrf.name = name #String
       new_vrf.save
     end
     
     def add_vid(vid, name)
+      # Create new VID
       new_vid = NetboxClientRuby::IPAM::Vid.new
       new_vid.vid = vid #integer
       new_vid.name = name #name
@@ -78,10 +84,23 @@ module Proxy::Netbox
     end
     
     def add_vlan_group(name, slug)
+      # Create new VLAN Group
       new_vlan_group = NetboxClientRuby::VlanGroup.new
       new_vlan_group.name = name #String
       new_vlan_group.slug = slug #String
       new_vlan_group.save
+    end
+    
+    def remove_ip(id)
+      # Remove an IP Address with the given ID
+      old_ip = NetboxClientRuby.ipam.ip(id)
+      old_ip.delete
+    end
+    
+    def remove_prefix(id)
+      # Remove the Prefix with the given ID
+      old_prefix = NetboxClientRuby.ipam.prefix(id)
+      old_prefix.remove
     end
   end
 end
