abfdb090fb6c6cdfa5b5f15ec27d759188ebb3b0
stephen-nelsonjr <47092375+stephen-nelsonjr@users.noreply.github.com>
2019-08-02 13:41:54 -0400

    Update netbox_client.rb

--- lib/smart_proxy_netbox/netbox_client.rb
+++ lib/smart_proxy_netbox/netbox_client.rb
@@ -22,7 +22,7 @@ module Proxy::Netbox
       
       NetboxClientRuby.configure do |c|
         c.netbox.auth.token = '0123456789abcdef0123456789abcdef01234567' #@netbox_config[:token]
-        c.netbox.api_base_url = 'http://172.16.13.33:8080/api/'#@api_base_address
+        c.netbox.api_base_url = 'http://172.16.13.33:8888/api/'#@api_base_address
         c.netbox.auth.rsa_private_key.path = '~/.ssh/netbox_rsa'
       end
       
